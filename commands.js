0caa0e27681e18d48917ccb47229711e70e0481a
RadicalGreen <164906924+RadicalGreen@users.noreply.github.com>
2026-06-22 22:34:54 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -60,6 +60,8 @@ module.exports.vpnLocked = false;
 const whitelist = [
 	"https://files.catbox.moe",
 	"https://i.ibb.co",
+	"https://i.imgur.com",
+	"https://imgur.com",
 ];
 module.exports.whitelist = whitelist;
 setInterval(()=>{module.exports.bancount = 0}, 60000*5)
